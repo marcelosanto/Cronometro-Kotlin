fd54d936fffaf4beec9153a1763ff1be6e6de505
Marcelo Santos <rocha.mer21@gmail.com>
2021-04-09 11:25:47 -0300

    feat: function iniciar and pause of chronometre

--- app/src/main/java/com/marcelo/cronometro/MainActivity.kt
+++ app/src/main/java/com/marcelo/cronometro/MainActivity.kt
@@ -2,19 +2,44 @@ package com.marcelo.cronometro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
 import com.marcelo.cronometro.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    var running = false
+    var pause: Long = 0
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        botaoIninciar = binding.btnIniciar
-        botaoPausar = binding.btnPausar
-        botaoReiniciar = binding.btnReiniciar
+        binding.btnIniciar.setOnClickListener {
+            IniciarCronometro()
+        }
+
+        binding.btnPausar.setOnClickListener {
+            PausarCronometro()
+        }
+    }
+
+    private fun IniciarCronometro(){
+        if(!running){
+            binding.cronometro.base = SystemClock.elapsedRealtime() - pause
+            binding.cronometro.start()
+            running = true
+        }
+    }
+
+    private fun PausarCronometro() {
+        if(running) {
+            binding.cronometro.stop()
+            pause = SystemClock.elapsedRealtime() - binding.cronometro.base
+            running = false
+        }
     }
 }
